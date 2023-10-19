6c7a3eb664a334b0ba5d9730dead4c4c5288d1cc
Ёлка <wongingdonyu@gmail.com>
2023-10-19 19:17:57 +0300

    some commit in dev4

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
+import java.util.Random;
+
 class Main{
     public static void main(String[] args) {
-        while (true){
-            System.out.println("Я хз чё ещё сюда писать");
+        for(int i=0;i<10;i++){
+            System.out.println(new Random().nextInt(-100, 100));
         }
     }
 }
