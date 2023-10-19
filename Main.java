16b531d2c7b7c4bf84e9a94e45e60641b77bd990
Ёлка <wongingdonyu@gmail.com>
2023-10-19 18:58:20 +0300

    some commit6

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 class Main{
     public static void main(String[] args) {
-        System.out.println("Сейчас время во:" + LocalDate.now());
+        System.out.println("ЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭХ ПИТОН ЛЯ");
+        System.out.print("Чёто я тупанул... Ща время во:");
+        System.out.print(LocalDateTime.now());
     }
 }
