e6285ab5e9e9bb751b72198da8d4034dd93fcf3b
Ёлка <wongingdonyu@gmail.com>
2023-10-19 18:52:32 +0300

    some commit3

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 class Main{
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int z = scanner.nextInt();
         System.out.print("Это добавлено в ветке main");
     }
 }
