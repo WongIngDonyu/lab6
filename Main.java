faddbb6d745217246660d3caeb55bf34b1b4c332
Ёлка <wongingdonyu@gmail.com>
2023-10-19 19:18:44 +0300

    some commit in dev5

--- Main.java
+++ Main.java
@@ -2,8 +2,11 @@ import java.util.Random;
 
 class Main{
     public static void main(String[] args) {
-        for(int i=0;i<10;i++){
-            System.out.println(new Random().nextInt(-100, 100));
-        }
+        int z=1;
+        z=2;
+        z=3;
+        z=4;
+        z=5;
+        System.out.println("Не пон, а где v ?");
     }
 }
