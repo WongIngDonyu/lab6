31789ce68c0f91f8fe116ebfc304721fdd6c12fb
Ёлка <wongingdonyu@gmail.com>
2023-10-19 18:56:08 +0300

    some commit5

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
+import java.time.LocalDate;
 import java.util.Scanner;
 
 class Main{
     public static void main(String[] args) {
-        System.out.println("Сканера не будет. Это Харам!!!!");
-        System.out.print("Это добавлено в ветке main");
+        System.out.println("Сейчас время во:" + LocalDate.now());
     }
 }
