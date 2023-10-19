2812e8b1d65bc5dfe3341879964e62edf2367080
Ёлка <wongingdonyu@gmail.com>
2023-10-19 19:19:28 +0300

    some commit in dev6

--- Main.java
+++ Main.java
@@ -2,11 +2,7 @@ import java.util.Random;
 
 class Main{
     public static void main(String[] args) {
-        int z=1;
-        z=2;
-        z=3;
-        z=4;
-        z=5;
-        System.out.println("Не пон, а где v ?");
+        System.out.print("Я ");
+        System.out.print("устал");
     }
 }
