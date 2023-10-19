ee0bb7cfe6dccf08f9a6973b070e4e697e7c52b4
Ёлка <wongingdonyu@gmail.com>
2023-10-19 19:12:22 +0300

    some commit in dev3

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main{
     public static void main(String[] args) {
-        System.out.print("Это добавлено в ветке dev");
+        while (true){
+            System.out.println("Я хз чё ещё сюда писать");
+        }
     }
 }
